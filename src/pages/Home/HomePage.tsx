ad05e0960b1e0dd8fae1ac1e1b63bb50321bd6c0
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-01-12 18:39:47 +0700

    Fix: Rename Home to HomePage

---
+++ src/pages/Home/HomePage.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import { RecipeList, IngredientList } from "../../components";
+
+const HomePage: React.FC = () => {
+  const [ingredients, setIngredients] = useState<string[]>([]);
+  const [recipes, setRecipes] = useState<string[]>([]);
+
+  const handleImageUpload = (file: File) => {
+    // TODO: Call backend to process the image and recognize ingredients
+    // Update the 'ingredients' state with the recognized ingredients
+    // Call another function to fetch recipes based on the recognized ingredients
+  };
+
+  const fetchRecipes = (recognizedIngredients: string[]) => {
+    // TODO: Call a recipe API to fetch recipes based on recognized ingredients
+    // Update the 'recipes' state with the fetched recipes
+  };
+
+  return (
+    <div>
+      <IngredientList
+        ingredients={ingredients}
+        onImageUpload={handleImageUpload}
+      />
+      <RecipeList recipes={recipes} />
+    </div>
+  );
+};
+
+export default HomePage;
