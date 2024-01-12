a71a6c859dccfe3ff8b7d7b684a2a5c7f6947a0f
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-01-12 16:58:55 +0700

    install packages

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-center">My Fridge</h1>
+    </div>
+  );
+};
+
+export default Home;
