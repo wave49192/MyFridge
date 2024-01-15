a80511af61deb42af10ef1c87c5905893e4ecc7a
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-01-15 21:32:06 +0700

    Add daisy UI

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   let color = "bg-red-500";
   return (
     <div>
-      <div className="{{ color }}">red</div>
+      <div className={color}>red</div>
 
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
