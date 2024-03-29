039f87d22fbfc073190b1dc5de4140a625889baa
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-03-29 20:27:49 +0700

    add responsive navbar

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,3 @@
-export { default as IngredientList } from "./IngredientList/IngredientList";
-export { default as FoodAndDrinkList } from "./FoodAndDrinkList/FoodAndDrinkList";
 export { default as RecipeList } from "./RecipeList/RecipeList";
-export { default as DashboardIngredients } from "./DashboardIngredients/DashboardIngredients";
-export { default as DashboardFoodAndDrinks } from "./DashboardFoodAndDrinks/DashboardFoodAndDrinks";
+
+export { default as Navbar } from "./Navbar/Navbar";
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,90 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
-import {
-  RecipeList,
-  IngredientList,
-  FoodAndDrinkList,
-  DashboardIngredients,
-  DashboardFoodAndDrinks,
-} from "../../components";
-import { MdFastfood } from "react-icons/md";
-import { MdSpaceDashboard } from "react-icons/md";
-import { BiFridge } from "react-icons/bi";
-import { BiSolidFridge } from "react-icons/bi";
-import { BiSolidFoodMenu } from "react-icons/bi";
+import { Navbar, RecipeList } from "../../components";
+
 const HomePage: React.FC = () => {
-  const [ingredients, setIngredients] = useState<string[]>([]);
-  const [items, setItems] = useState<string[]>([]);
-  const [recipes, setRecipes] = useState<string[]>([]);
-
-  const handleImageUpload = (file: File) => {};
-
-  const fetchRecipes = (recognizedIngredients: string[]) => {};
-
   return (
     <div>
-      <div className="drawer lg:drawer-open ">
-        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex flex-col">
-          <DashboardIngredients
-            ingredients={ingredients}
-            onImageUpload={handleImageUpload}
-          />
-          <DashboardFoodAndDrinks
-            items={items}
-            onImageUpload={handleImageUpload}
-          />
-          <RecipeList recipes={recipes} />
-
-          <label
-            htmlFor="my-drawer-2"
-            className="btn btn-primary drawer-button lg:hidden"
-          >
-            Open drawer
-          </label>
-        </div>
-        <div className="drawer-side">
-          <label
-            htmlFor="my-drawer-2"
-            aria-label="close sidebar"
-            className="drawer-overlay"
-          ></label>
-          <ul className="menu  p-4 w-80 min-h-full bg-base-200 text-base-content font-bold">
-            {/* Sidebar content here */}
-
-            <a className="flex items-center ml-10 mt-5 mb-5">
-              <BiFridge size={50} />
-              <p className="text-4xl cursor-default">MyFridge</p>
-            </a>
-            <li></li>
-            <a className="flex items-center">
-              <p className="text-slate-600 font-bold ml-4 my-1">Menu</p>
-            </a>
-            <li>
-              <a className="a-tag-style">
-                <MdSpaceDashboard size={25} className="mr-2" />
-                Dashboard
-              </a>
-            </li>
-            <li></li>
-            <a className="flex items-center">
-              <p className="text-slate-600 font-bold ml-4 my-1">Content</p>
-            </a>
-            <li>
-              <a className="a-tag-style">
-                <BiSolidFridge size={25} className="mr-2" />
-                Refrigerator
-              </a>
-            </li>
-            <li>
-              <a className="a-tag-style">
-                <BiSolidFoodMenu size={25} className="mr-2" />
-                Recipe Generator
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Navbar />
+      {/* <RecipeList /> */}
     </div>
   );
 };
