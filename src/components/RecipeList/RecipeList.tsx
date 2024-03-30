b2b194ecf0e559639ee6d5f12af6d7a602441a11
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-03-30 17:36:16 +0700

    update path

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface Recipe {
   recipe_id: string;
@@ -10,7 +11,6 @@ interface Recipe {
   ingredients: string[];
   cuisine_type: string;
 }
-
 interface RecipeListProps {
   loading: boolean;
   recipes: Recipe[];
@@ -57,44 +57,43 @@ const RecipeList: React.FC<RecipeListProps> = ({
   return (
     <div className="pb-4">
       {recipes.map((recipe) => (
-        <div
-          key={recipe.recipe_id}
-          className="card card-side bg-base-100 m-2 h-48 "
-        >
-          <figure>
-            <img
-              src={recipe.image_url}
-              alt={recipe.title}
-              className="rounded-2xl mobile:w-32 mobile:h-48"
-              onError={(e) => {
-                // Display placeholder image if original image fails to load
-                e.currentTarget.src = placeholderImage;
-              }}
-            />
-          </figure>
-          <div className="pl-3 w-[239px] flex-col justify-end">
-            <div>
-              <h2 className="text-xl font-bold text-secondary-green text-ellipsis overflow-hidden line-clamp-2">
-                {recipe.title}
-              </h2>
-              <div className="badge badge-primary text-white">
-                {recipe.cuisine_type}
+        <Link to={`/recipe/${recipe.recipe_id}`} key={recipe.recipe_id}>
+          <div className="card card-side bg-base-100 m-2 h-48">
+            <figure>
+              <img
+                src={recipe.image_url}
+                alt={recipe.title}
+                className="rounded-2xl mobile:w-32 mobile:h-48"
+                onError={(e) => {
+                  // Display placeholder image if original image fails to load
+                  e.currentTarget.src = placeholderImage;
+                }}
+              />
+            </figure>
+            <div className="pl-3 w-[239px] flex-col justify-end">
+              <div>
+                <h2 className="text-xl font-bold text-secondary-green text-ellipsis overflow-hidden line-clamp-2">
+                  {recipe.title}
+                </h2>
+                <div className="badge badge-primary text-white">
+                  {recipe.cuisine_type}
+                </div>
+                <div className="flex-1">
+                  <p className="text-ellipsis overflow-hidden line-clamp-4">
+                    Succulent salmon fillets seasoned with aromatic herbs,
+                    grilled to perfection, and finished with a zesty lemon herb
+                    butter sauce. The fish is tender and flaky, with a burst of
+                    fresh flavors.
+                  </p>
+                </div>
               </div>
-              <div className="flex-1">
-                <p className="text-ellipsis overflow-hidden line-clamp-4">
-                  Succulent salmon fillets seasoned with aromatic herbs, grilled
-                  to perfection, and finished with a zesty lemon herb butter
-                  sauce. The fish is tender and flaky, with a burst of fresh
-                  flavors.
-                </p>
-              </div>
+              <p className="flex-end text-xs text-end items-end">
+                Cooking Time:{" "}
+                <span className="text-primary">{recipe.cooking_time} mins</span>
+              </p>
             </div>
-            <p className="flex-end text-xs text-end items-end">
-              Cooking Time:{" "}
-              <span className="text-primary">{recipe.cooking_time} mins</span>
-            </p>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,49 +1,50 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import DashboardPage from "./pages/Dashboard/DashboardPage.tsx";
 import RecipesPage from "./pages/Recipes/RecipesPage.tsx";
 import HomePage from "./pages/Home/HomePage.tsx";
 import InventoryPage from "./pages/Inventory/InventoryPage.tsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <html data-theme="emmy">
-        <HomePage />
-      </html>
-    ),
-  },
-  {
-    path: "/dashboard",
-    element: (
-      <html data-theme="emmy">
-        <DashboardPage />
-      </html>
-    ),
-  },
-  {
-    path: "/recipes",
-    element: (
-      <html data-theme="emmy">
-        <RecipesPage />
-      </html>
-    ),
-  },
-  {
-    path: "/inventory",
-    element: (
-      <html data-theme="emmy">
-        <InventoryPage />
-      </html>
-    ),
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <html data-theme="emmy">
+              <HomePage />
+            </html>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <html data-theme="emmy">
+              <DashboardPage />
+            </html>
+          }
+        />
+        <Route
+          path="/recipes"
+          element={
+            <html data-theme="emmy">
+              <RecipesPage />
+            </html>
+          }
+        />
+        <Route
+          path="/inventory"
+          element={
+            <html data-theme="emmy">
+              <InventoryPage />
+            </html>
+          }
+        />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
