4f564f13fd7f19d42b663d773dce2dd73ba91422
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-03-30 20:33:31 +0700

    update style for recipe details page

--- src/pages/Recipes/RecipeDetailsPage.tsx
+++ src/pages/Recipes/RecipeDetailsPage.tsx
@@ -40,102 +40,74 @@ const RecipeDetailsPage: React.FC = () => {
   const ingredientsArray = recipe.ingredients.split(",");
 
   return (
-    <div className="flex flex-col items-center justify-center bg-white text-accent shadow-lg">
+    <div className="flex flex-col min-h-screen justify-between bg-white text-accent">
       <Navbar isAtPage="Recipes" />
-      <img
-        src={recipe.image_url}
-        alt={recipe.title}
-        className="w-full h-[300px] object-cover"
-      />
-      <h2 className="text-3xl font-bold mb-1 text-secondary-green">
-        {recipe.title}
-      </h2>
-      <p className="text-lg mb-5">
-        By<span className="text-primary"> {recipe.publisher}</span>
-      </p>
-      <div className="flex mb-3">
-        <div className="badge badge-primary p-3 mb-3 mr-3 text-white">
-          {recipe.cuisine_type}
-        </div>
-        <div className="badge badge-primary p-3 mb-3 mr-3 text-white">
-          {recipe.cuisine_type}
-        </div>
-        <div className="badge badge-primary p-3 mb-3 mr-3 text-white">
-          {recipe.cuisine_type}
-        </div>
-      </div>
-      <p className="text-md mb-2">
-        Cooking Time:{" "}
-        <span className="text-md text-primary">{recipe.cooking_time} mins</span>
-      </p>
-      <div className="card w-96 bg-base-100 shadow-xl bg-primary-2 bg-opacity-25">
-        <div className="card-body">
-          <div className="text-lg">
-            <div className="text-center mb-4 font-bold text-primary opacity-75">
-              RECIPE INGREDIENTS
+      <div className="flex-grow">
+        <img
+          src={recipe.image_url}
+          alt={recipe.title}
+          className="w-full h-[300px] object-cover"
+        />
+        <div className="p-5">
+          <h2 className="text-3xl font-bold mb-1 text-secondary-green">
+            {recipe.title}
+          </h2>
+          <p className="text-lg mb-5 font-semibold">
+            By<span className="text-primary"> {recipe.publisher}</span>
+          </p>
+          <div className="flex flex-wrap mb-3">
+            <div className="badge badge-primary p-3 mb-3 mr-3 text-white">
+              {recipe.cuisine_type}
+            </div>
+            <div className="badge badge-primary p-3 mb-3 mr-3 text-white">
+              {recipe.cuisine_type}
+            </div>
+            <div className="badge badge-primary p-3 mb-3 mr-3 text-white">
+              {recipe.cuisine_type}
             </div>
           </div>
-          <ul>
-            {ingredientsArray.map((ingredient, index) => (
-              <div key={index} className=" justify-between">
-                <p className="text-accent opacity-75">{ingredient}</p>
-                {index !== ingredientsArray.length && (
-                  <hr className="w-full border-t border-gray-200 my-1 opacity-20" />
-                )}
+          <p className="text-md mb-2 font-semibold">
+            Cooking Time:{" "}
+            <span className="text-md text-primary">
+              {recipe.cooking_time} mins
+            </span>
+          </p>
+          <div className="w-full md:w-96 bg-base-100 shadow-xl bg-primary-2 bg-opacity-25">
+            <div className="card-body">
+              <div className="text-lg">
+                <div className="text-center mb-4 font-bold text-primary opacity-75">
+                  RECIPE INGREDIENTS
+                </div>
               </div>
-            ))}
-          </ul>
+              <ul>
+                {ingredientsArray.map((ingredient, index) => (
+                  <div key={index} className="justify-between">
+                    <p className="text-accent opacity-75">{ingredient}</p>
+                    {index !== ingredientsArray.length - 1 && (
+                      <hr className="w-full border-t border-gray-200 my-1 opacity-20" />
+                    )}
+                  </div>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-      <footer className="footer p-10 bg-neutral text-neutral-content">
-        <aside>
-          <p>
-            ACME Industries Ltd.
-            <br />
-            Providing reliable tech since 1992
+      <footer className="p-8 bg-secondary text-white bg-opacity-85">
+        <div className="max-w-screen-md mx-auto">
+          <h6 className="text-center text-white font-bold mb-3">
+            STEP TO COOK
+          </h6>
+          <p className="laptop:text-center">
+            This recipe was crafted and verified by the culinary experts. For
+            detailed instructions, please visit their website.
           </p>
-        </aside>
-        <nav>
-          <h6 className="footer-title">Social</h6>
-          <div className="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path
-                  d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.
-266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
-                ></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
+          <div className="mt-4 flex justify-center">
+            <a className="btn btn-primary text-white" href={recipe.source_url}>
+              Click Here
             </a>
           </div>
-        </nav>
+        </div>
       </footer>
     </div>
   );
