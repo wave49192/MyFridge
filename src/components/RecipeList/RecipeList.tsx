597767154dfbce26a30d274c7ed2419890184b5f
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-03-30 23:20:55 +0700

    remove shuffle

--- src/components/RecipeList/RecipeList.tsx
+++ src/components/RecipeList/RecipeList.tsx
@@ -11,7 +11,6 @@ interface Recipe {
   ingredients: string[];
   cuisine_type: string;
 }
-
 interface RecipeListProps {
   loading: boolean;
   recipes: Recipe[];
@@ -23,15 +22,6 @@ const RecipeList: React.FC<RecipeListProps> = ({
   recipes,
   placeholderImage,
 }) => {
-  // Function to shuffle array elements
-  const shuffleArray = (array: any[]) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  };
-
   if (loading) {
     return (
       <div className="m-2 flex justify-center">
@@ -64,12 +54,9 @@ const RecipeList: React.FC<RecipeListProps> = ({
     );
   }
 
-  // Shuffle recipes array
-  const shuffledRecipes = shuffleArray([...recipes]);
-
   return (
     <div className="pb-4">
-      {shuffledRecipes.map((recipe) => (
+      {recipes.map((recipe) => (
         <Link to={`/recipe/${recipe.recipe_id}`} key={recipe.recipe_id}>
           <div className="card card-side bg-base-100 m-2 h-48">
             <figure>
