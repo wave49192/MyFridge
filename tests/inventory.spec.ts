51989bc1177d04813d1547865dc9cec942e3eb5c
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2024-04-09 22:56:06 +0700

    add inventory test file

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -1,47 +1,23 @@
 import { test, expect } from "@playwright/test";
-
-test("inventory page show object detect feature section", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:5173");
 });
 
-test("try feature section should navigate to detect page", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("inventory page has section to show ingredients in fridge", async ({
-  page,
-}) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("add ingredient should navigate to add ingredient page", async ({
-  page,
-}) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("pagination should work normally", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test("when fridge not found should navigate to home", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test("Inventory page should render correctly", async ({ page }) => {
+  await page.goto("http://localhost:5173/inventory");
+  await page.waitForLoadState("networkidle");
+  const pageTitle = await page.textContent("h2");
+  expect(pageTitle).toContain("Inventory");
+  const searchBar = await page.waitForSelector("input[type='text']");
+  expect(searchBar).not.toBeNull();
+  const tryFeature = await page.waitForSelector(
+    'div:has-text("AI Detecting Ingredients!")'
+  );
+  expect(tryFeature).not.toBeNull();
+  const pagination = await page.waitForSelector('div:has-text("1 of 10")');
+  expect(pagination).not.toBeNull();
+  const addIngredientButton = await page.waitForSelector(
+    "a[href='/inventory/add']"
+  );
+  expect(addIngredientButton).not.toBeNull();
 });
